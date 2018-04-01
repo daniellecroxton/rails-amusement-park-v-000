8bb9931ca837d1a43f7da23ec369cc18fb098768
daniellecroxton <danikrist@gmail.com>
2018-03-31 22:34:22 -0600

    Done.

--- app/controllers/concerns/attractions_controller.rb
+++ app/controllers/concerns/attractions_controller.rb
@@ -4,11 +4,32 @@ class AttractionsController < ApplicationController
   end
 
   def show
-    # if logged_in?
-      @attraction = Attraction.find_by_id(params[:id])
-      @user = User.find_by_id(session[:user_id])
-    # else
-    #   redirect_to root_path
-    # end
+    @attraction = Attraction.find_by_id(params[:id])
+    @ride = Ride.new
+  end
+
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.create(attraction_params)
+      redirect_to attraction_path(@attraction)
+  end
+
+  def edit
+    @attraction = Attraction.find_by_id(params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find_by_id(params[:id])
+    @attraction.update(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :password, :nausea_rating, :happiness_rating, :tickets, :min_height)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :attractions
+  post "/rides/new", to: "rides#new"
   root 'users#index'
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
